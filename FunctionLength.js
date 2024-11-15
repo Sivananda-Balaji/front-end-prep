2762b16505ebc7ffdd2b0cd8a45224033e8b357d
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2024-11-15 06:53:10 +0530

    add function length solution

---
+++ FunctionLength.js
@@ -0,0 +1,13 @@
+//Function Length
+
+function functionLength(fn) {
+  return fn.length;
+}
+
+function foo() {}
+function bar(a) {}
+function baz(a, b) {}
+
+console.log(functionLength(foo)); // 0
+console.log(functionLength(bar)); // 1
+console.log(functionLength(baz)); // 2
